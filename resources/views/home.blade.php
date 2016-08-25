28fed3f058b691860c345295c61720c7190bfe09
AVGorbunov <immarketing@yandex.ru>
2016-08-25 15:40:38 +0300

    222

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -34,18 +34,8 @@
         </div>
 
         <div class="col-md-6" >
-            <div class="panel-group">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <h4 class="panel-title">
-                            <a data-toggle="collapse" href="#collapse5">Состав обущающихся</a>
-                        </h4>
-                    </div>
-                    <div id="collapse5" class="panel-collapse collapse">
-                        <div class="panel-body">Panel Body</div>
-                        <div class="panel-footer">Panel Footer</div>
-                    </div>
-                </div>
+            <div>
+                Состав обучающихся
             </div>
 
         </div>
